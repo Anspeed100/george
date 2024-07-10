76dec4c9576e2e03b626fd3ba5f2ad3d00bac73c
Andy <andykorshie@yahoo.com>
2024-07-10 20:59:38 +0000

    Repo Set up

--- src/pages/events/index.jsx
+++ src/pages/events/index.jsx
@@ -40,7 +40,7 @@ export default function AboutPage(){
   useEffect(() => {
     setIsLoading(true)
     const fetchData = async () => {
-      const sheetId = '1N6E4SXmY38agoGfr23YDg98wZlB2U_Z4d8xDevQlPM8';
+      const sheetId = 'id';
       const apiKey = process.env.REACT_APP_MASTER_KEY;
       const range = 'Sheet3';
       
